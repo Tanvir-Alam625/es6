b05696c4974aab8437961538a9f6756864d3efdc
tanveer <mdtanviralamht731@gmail.com>
2022-02-21 03:01:30 +0600

    class file modified

--- class.js
+++ class.js
@@ -3,9 +3,14 @@ class Study {
     roll = 123;
     age = 16;
     marks = 78;
+    constructor (name, mark) {
+        this.name = name;
+        this.marks = mark;
+    }
     myFunction (){
         console.log('Barguna Polytechnic institute');
     }
 }
-const Tanvir  = new Study();
+const Tanvir  = new Study('Md. Tanvir Alam', 90);
+Tanvir.myFunction()
 console.log(Tanvir);
