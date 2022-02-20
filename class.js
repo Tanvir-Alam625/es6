3f332331717a6f57ffe93ab755effe55fd74eb16
tanveer <mdtanviralamht731@gmail.com>
2022-02-21 02:57:09 +0600

    class file added

---
+++ class.js
@@ -0,0 +1,11 @@
+class Study {
+    name;
+    roll = 123;
+    age = 16;
+    marks = 78;
+    myFunction (){
+        console.log('Barguna Polytechnic institute');
+    }
+}
+const Tanvir  = new Study();
+console.log(Tanvir);
---
+++ map.js
@@ -0,0 +1,3 @@
+const fewNumbers = [45,56,34,5,4,3,545,335,6433,56634,5335,6645,6];
+const maxNumber = fewNumbers.map(number => number * number)
+console.log(maxNumber);
