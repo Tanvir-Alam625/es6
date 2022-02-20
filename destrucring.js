693b35c31587edb000aa54dd81df438f01ae7614
tanveer <mdtanviralamht731@gmail.com>
2022-02-20 23:57:21 +0600

    object destrucring added

--- destrucring.js
+++ destrucring.js
@@ -1,3 +1,23 @@
+
+// array
 const myArray= [ 234,325,23,53,34,5,2,3,3,5566,2];
 const [first, second, third] = myArray;
-console.log(second);
+// console.log(second);
+// object 
+const laptop = {
+    names: 'hp',
+    RAM: '8gb',
+    storage: '1tb',
+    ssd: '128gb'
+}
+const {names, RAM} = laptop;
+// console.log(RAM);
+const phoneDetails = {
+    brand:{
+        name: 'Nokina',
+        price: 4000,
+        guranty: '2 Year'
+    }
+}
+const {name, guranty} = phoneDetails.brand;
+console.log(guranty);
