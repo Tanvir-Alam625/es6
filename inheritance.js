69d404050fb633cbd62d5cc3cd6d201c95f71396
tanveer <mdtanviralamht731@gmail.com>
2022-02-21 03:44:07 +0600

    inheritance file added

---
+++ inheritance.js
@@ -0,0 +1,74 @@
+// common class 
+class MyProducts {
+    name;
+    price = 50000;
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+
+    }
+
+}
+// laptop class 
+class laptop extends MyProducts {
+    Ram = '4gb';
+    constructor(name, price, Ram){
+        super(name, price,  Ram)
+        this.Ram = Ram;
+
+    }
+    myFunction(version){
+        console.log(this.name, 'yeah! I am release', version);
+    }
+}
+// phone class 
+class phone extends MyProducts {
+    color = 'lightblue';
+    constructor(name, price, color){
+        super(name, );
+        this.price = price;
+        this.color = color;
+    }
+    myFunction(version){
+        console.log(this.name, 'yeah! I am release', version);
+    }
+}
+// watch class 
+class watch {
+    name;
+    weight = '2kg';
+    price = 50000;
+    from = 'USA'
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+
+    }
+    myFunction(version){
+        console.log(this.name, 'yeah! I am release', version);
+    }
+}
+// modem class 
+class modem {
+    name;
+    weight = '2kg';
+    price = 50000;
+    from = 'USA'
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+
+    }
+    myFunction(version){
+        console.log(this.name, 'yeah! I am release', version);
+    }
+}
+// calling 
+// laptop
+// const myLaptop = new laptop('hp',60000 ,'16gb');
+// myLaptop.myFunction('12.23.2')
+// console.log(myLaptop);
+//phone 
+const myPhone = new phone('samsung', 12000, 'green');
+console.log(myPhone);
+
